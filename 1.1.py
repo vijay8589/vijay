d7bc2fa900fc114501b0a6a911fe8cfafabf752b
vijay8589 <113516660+vijay8589@users.noreply.github.com>
2022-09-15 19:22:18 +0530

    Add files via upload

---
+++ 1.1.py
@@ -0,0 +1,13 @@
+"""print the first 10 natural numbers using for loop"""
+a = 10
+for i in range(0, a):  # for loop start with zero index.
+    print(i)
+
+    
+
+"""Python program to print all the even numbers within the given range is 10."""
+
+a = 10
+for i in range(0, a):
+    if i % 2 == 0:  # even numbers or divisible by 2
+        print(i)
