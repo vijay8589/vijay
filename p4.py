ae9963f832827b717e8d4755e164534a20d81cbe
vijay8589 <113516660+vijay8589@users.noreply.github.com>
2022-09-17 18:49:19 +0530

    Add files via upload

---
+++ p4.py
@@ -0,0 +1,39 @@
+""" (Right-angled Triangle) Pattern of Numbers
+Pattern:
+
+           1
+
+          1 2
+
+        1 2 3
+
+       1 2 3 4
+
+      1 2 3 4 5"""
+
+
+rows = 6
+
+for row in range(1, rows):
+
+    num = 1
+
+    for j in range(rows, 0, -1):
+
+        if j > row:
+
+            print(" ", end=" ")
+
+        else:
+
+            print(num, end=" ")
+
+            num += 1
+
+    print(" ")
+
+
+
+
+
+
