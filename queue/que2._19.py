4e9a02a0dfbda982f2ec83f757a17fd25c517771
vijay8589 <113516660+vijay8589@users.noreply.github.com>
2022-09-19 20:07:16 +0530

    Add files via upload

---
+++ queue/que2._19.py
@@ -0,0 +1,28 @@
+# queue implimentation in list
+
+queue = []
+def enqueue():
+    element = input("enter the element:")
+    queue.append(element)
+    print(element,"is add to queue")
+def dequeue():
+    if not queue:
+        print("queue is empty")
+    else:
+        e = queue.pop(0)
+        print("removed element:",e)
+def display():
+    print(queue)
+while True:
+    print("selected the operation 1.add, 2.remove, 3.show, 4.quit")
+    choice = int(input())
+    if choice == 1:
+            enqueue()
+    elif choice == 2:
+        dequeue()
+    elif choice == 3:
+        display()
+    elif choice == 4:
+        break
+    else:
+        print("enter the correct operation")
