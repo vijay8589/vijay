027c85a0f67bccc816ac9fd11d09d401c463f40d
vijay8589 <113516660+vijay8589@users.noreply.github.com>
2022-09-19 19:53:34 +0530

    Add files via upload

---
+++ pattern problems/pattern_2.py
@@ -0,0 +1,25 @@
+"""Even Number Pyramid Pattern
+10
+10 8
+10 8 6
+10 8 6 4
+10 8 6 4 2"""
+
+
+rows = 5
+
+LastEvenNumber = 2 * rows
+
+evenNumber = LastEvenNumber
+
+for i in range(1, rows+1):
+
+    evenNumber = LastEvenNumber
+
+    for j in range(i):
+
+        print(evenNumber, end=" ")
+
+        evenNumber -= 2
+
+    print("\r")
