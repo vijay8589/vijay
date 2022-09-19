cfb02c133c4ff80583d09aa0021fea154813f69c
vijay8589 <113516660+vijay8589@users.noreply.github.com>
2022-09-19 19:49:45 +0530

    Add files via upload

---
+++ list and tuple/list_3.py
@@ -0,0 +1,39 @@
+# Different types of tuples
+
+# Empty tuple
+my_tuple = ()
+print(my_tuple)
+
+# Tuple having integers
+my_tuple = (1, 2, 3)
+print(my_tuple)
+
+# tuple with mixed datatypes
+my_tuple = (1, "vijay", 3.4)
+print(my_tuple)
+
+# tuple
+my_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
+print(my_tuple)
+
+
+
+
+# indexing
+
+# the tuple elements using indexing
+my_tuple = ('p','e','r','m','i','t')
+
+print(my_tuple[0])   # 'p'
+print(my_tuple[5])   # 't'
+
+# negative indexing
+
+
+my_tuple = ('v', 'i', 'j', 'a', 'y')
+
+# Output: 'y'
+print(my_tuple[-1])
+
+# Output: 'v'
+print(my_tuple[-5])
