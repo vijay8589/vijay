ae194eff4acfb1c7701cacf35587a035cd5e511b
vijay8589 <113516660+vijay8589@users.noreply.github.com>
2022-09-19 20:06:53 +0530

    Add files via upload

---
+++ queue/que1.19.py
@@ -0,0 +1,20 @@
+
+# add thd elements in queue in list
+queue = []
+queue.append(10)
+queue.append(12)
+queue.append(15)
+queue.append(20)
+queue.append(25)
+print(queue)
+
+# remove the elements in queue in list
+queue.pop(0)
+print(queue)
+queue.pop(2)
+print(queue)
+
+
+
+
+
