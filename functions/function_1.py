64944e6625824797a6faccb08cdadb161c460710
vijay8589 <113516660+vijay8589@users.noreply.github.com>
2022-09-19 19:58:31 +0530

    Add files via upload

---
+++ functions/function_1.py
@@ -0,0 +1,36 @@
+"""Create a function"""
+
+
+# demo is the function name
+def demo(name, age):
+    # print value
+    print(name, age)
+
+# call function
+demo("Ben", 25)
+
+
+
+"""Create a function with variable length of arguments"""
+
+
+def func1(*args):
+    for i in args:
+        print(i)
+
+func1(20, 40, 60)
+func1(80, 100)
+
+
+"""Return multiple values from a function"""
+
+
+def calculation(a, b):
+    addition = a + b
+    subtraction = a - b
+
+    return addition, subtraction
+
+# get result in tuple format
+res = calculation(40, 10)
+print(res)
