a23cef407d3530ffec7e10edbc4e3dbee027628f
vijay8589 <113516660+vijay8589@users.noreply.github.com>
2022-10-04 07:00:18 +0530

    Add files via upload

---
+++ sorting/insertion sort_1.py
@@ -0,0 +1,17 @@
+"""Write a Python program to sort a list of elements using the insertion sort."""
+
+def insertionSort(nlist):
+   for index in range(1,len(nlist)):
+
+     currentvalue = nlist[index]
+     position = index
+
+     while position>0 and nlist[position-1]>currentvalue:
+         nlist[position]=nlist[position-1]
+         position = position-1
+
+     nlist[position]=currentvalue
+
+nlist = [14,46,43,27,57,41,45,21,70]
+insertionSort(nlist)
+print(nlist)
