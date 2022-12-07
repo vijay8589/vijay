99e9efef1502bdf744b1869a5ed441e91e867873
vijay8589 <113516660+vijay8589@users.noreply.github.com>
2022-12-07 11:23:30 +0530

    Add files via upload

---
+++ test_cases/test_case1.py
@@ -0,0 +1,16 @@
+
+import math
+
+def test_sqrt():
+   num = 625
+   assert math.sqrt(num) == 25
+
+def testsquare():
+   num = 7
+   assert 7*7 == 64
+
+
+
+# code is run in the terminal use the pytest <give the file name>
+
+# and also use the pytest <file name> -v    "here -v is the increase the verbosity"   
